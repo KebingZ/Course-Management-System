0327bffe008c13cf7f37f3aca045dadb1cade7f4
KebingZ <zkbrichard@gmail.com>
2022-05-23 22:55:47 +0800

    update

--- src/component/login.js
+++ src/component/login.js
@@ -6,54 +6,6 @@ import Icon, { UserOutlined, LockOutlined } from '@ant-design/icons';
 import CryptoJS from "crypto-js";
 import axios from "axios";
 
-// class Login extends React.Component {
-    
-//     render() {
-//         const { getFieldDecorator } = this.props.form;
-//         const handleSubmit = (e) => {
-//               e.preventDefault();
-//               this.props.form.validateFields((err, values) => {
-//                 if (!err) {
-//                   console.log('Received values of form: ', values);
-//                 }
-//               });
-//             }
-//       return (
-//         <Form onSubmit={this.handleSubmit} className="login-form">
-//           <Form.Item>
-//             {getFieldDecorator('userName', {
-//               rules: [{ required: true, message: 'Please input your username!' }],
-//             })(
-//               <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-//             )}
-//           </Form.Item>
-//           <Form.Item>
-//             {getFieldDecorator('password', {
-//               rules: [{ required: true, message: 'Please input your Password!' }],
-//             })(
-//               <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-//             )}
-//           </Form.Item>
-//           <Form.Item>
-//             {getFieldDecorator('remember', {
-//               valuePropName: 'checked',
-//               initialValue: true,
-//             })(
-//               <Checkbox>Remember me</Checkbox>
-//             )}
-//             <a className="login-form-forgot" >Forgot password</a>
-//             <Button type="primary" htmlType="submit" className="login-form-button">
-//               Log in
-//             </Button>
-//             Or <a >register now!</a>
-//           </Form.Item>
-//         </Form>
-//       );
-//     }
-//   }
-  
-//   export default Login;
-
 export default function Login() {
     const [form] = Form.useForm();
     const [ password, setPassword ] = useState("");
